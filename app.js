ad687a824371983622b19abd41af1dba0c22f407
James Jenkins <jamesjenkinsjr@icloud.com>
2018-01-30 20:45:02 -0500

    Complete random name generation and updated page

--- app.js
+++ app.js
@@ -2,13 +2,28 @@
 const button = document.querySelector('#generate');
 const firstString = document.querySelector('#first');
 const lastString = document.querySelector('#last');
-
+const firstStringArray = [
+    'Neo',
+    'Morpheus',
+    'Trinity'
+];
+const lastStringArray = [
+    'Gorgonzola',
+    'Cheddar',
+    'Havarti'
+];
 //Create onclick event
 button.addEventListener('click', generate);
 
 //Generate the response
 function generate (e) {
     //Fill #first and #last h2 elements with random strings
-    console.log(firstString, lastString);
-
+    const randomFirstString = randomStringSelect(firstStringArray);
+    const randomLastString = randomStringSelect(lastStringArray);
+    firstString.textContent = randomFirstString;
+    lastString.textContent = randomLastString;
+}
+function randomStringSelect (array) {
+    const rndIndex = Math.floor(Math.random() * array.length);
+    return array[rndIndex];
 }
